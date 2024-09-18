fe7e216cc9afe3c9105794cffe930fa5dfc669b3
shivanisharm07 <155681997+shivani7405@users.noreply.github.com>
2024-09-18 13:48:10 +0000

    file 2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 42484
+let accountEmail = "shivanisharma@gmail.com"
+var accountPassword = 57230
+accountCity = "Jaipur"
+
+// accountId = 2385709 // wrong because constant value can not be changed
+
+
+// we do not use let possible we only use var
+// console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
